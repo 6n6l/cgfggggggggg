f7497b1284e340c45deb7b27f37df9bfea8328fb
6n6l <43895200+6n6l@users.noreply.github.com>
2019-10-25 15:01:26 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,52 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = "-"
+
+client.on('ready', () => {
+	// Cmd Sending in Console
+	  console.log('')
+	  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+	  console.log('╔                           BroadCast                               ╗')
+	  console.log('')
+	  console.log('╔ Best Bot in 2019 and 2040 ╗')
+	  console.log(`[Start] ${new Date()}`)
+	  console.log(`╔[ Logged in as * [ " ${client.user.username} " ] ]?`);
+	  console.log(`╔[ Servers [ " ${client.guilds.size} " ]╗`);
+	  console.log(`╔[ Users [ " ${client.users.size} " ]╗`);
+	  console.log(`╔[ Channels [ " ${client.channels.size} " ]╗`);
+	  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+      console.log('')
+      client.user.setGame('Made With ♥ by TheBeatz'); 
+    });
+
+client.on("message", message => {
+    if (message.content.startsWith("obc")) {
+                 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' ');
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+  m.send(`${argresult}\n ${m}`);
+  })
+  message.channel.send(`\`${message.guild.members.filter( m => m.presence.status !== 'all').size}\`:mailbox:  عدد المستلمين `);
+  message.delete();
+  };
+  });
+
+//bc online
+
+  client.on("message", message => {
+  
+              if (message.content.startsWith(prefix + "bc")) {
+                           if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+    let args = message.content.split(" ").slice(1);
+    var argresult = args.join(' '); 
+    message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+   m.send(`${argresult}\n ${m}`);
+  })
+   message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` :mailbox:  عدد المستلمين `); 
+   message.delete(); 
+  };     
+  });
+
+
+  client.login('NDcwNjc3ODI0NDI5MjI4MDM0.XbLitg.P58ijFZ7Tb236mgqJJvpBu2hmPY')
